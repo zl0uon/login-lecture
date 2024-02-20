ab04ab5d13fcfb6e43b5f25857f5d813cd3c5953
apple <yjiyeong113@gmail.com>
2024-02-20 21:01:12 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,19 @@
+// java script 엄격 모드(문법 검사 강화)
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// 라우팅
+const home = require("./routes/home");
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); //use-> 미들 웨어를 등록해주는 메서드
+
+module.exports = app;
